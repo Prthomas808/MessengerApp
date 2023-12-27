b85cba5054b8eb688e9957af85553fa8d506911c
Pedro <prthomas808@gmail.com>
2023-12-27 14:38:22 -0500

    Initial Commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Pedro Thomas on 12/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
